2c40004aec4312983eb8f2287834debc90e4d560
Luka Amiranashvili <luka.amiranashvili.2001@gmail.com>
2025-06-08 00:33:54 +0400

    changed dynamic after client

--- src/app/thank-you/page.js
+++ src/app/thank-you/page.js
@@ -1,5 +1,5 @@
-export const dynamic = "force-dynamic";
 ("use client");
+export const dynamic = "force-dynamic";
 
 import { useSearchParams } from "next/navigation";
 import { useLanguage } from "../../context/LanguageContext";
