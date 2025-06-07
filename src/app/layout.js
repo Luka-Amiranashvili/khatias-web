45d0df952a668909ef9754d64086dd53df0d1d8a
Luka Amiranashvili <luka.amiranashvili.2001@gmail.com>
2025-06-07 23:51:43 +0400

    changed layout to client

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,11 @@
+"use client";
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { LanguageProvider } from "@/context/LanguageContext";
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +25,7 @@ export default function RootLayout({ children }) {
       >
         <LanguageProvider>
           <Navbar />
-          {children}
+          <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
           <Footer />
         </LanguageProvider>
       </body>
